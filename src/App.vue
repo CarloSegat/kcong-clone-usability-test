ed42e5ef1392f98d6b3bf098a25a25cba9407d65
Carlo Segat <segatcarlo95@gmail.com>
2022-07-15 19:07:47 +0200

    navbar on root componetn outside router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <main>
+    <NavBar></NavBar>
+    <br>
+    <br>
     <router-view></router-view>
   </main>
   <span style="font-size: 3em; color: Tomato;">
       <i class="fas fa-camera"></i>
     </span>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
 
 <style>
 @import './assets/base.css';
---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+ <ul class='navbar'>
+  <img 
+    style='width: 3rem;'
+    src="https://pbs.twimg.com/profile_images/784383890865651712/pG1YAsHt_400x400.jpg">
+  <span class='navElement'>Datasets Metadata Catalogue</span>
+  <span
+    style='margin-left: auto; margin-right: 3rem;'
+    @click="e => this.$router.push({ 
+            name: 'Home'
+      })"
+  >
+    HOME
+  </span>
+</ul>
+</template>
+
+<script lang="js">
+
+export default {
+  name: "NavBar"
+}
+
+</script>
+
+<style scoped>
+
+.navbar {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  top: 0rem;
+  left: 0rem;
+  background-color: rgb(242, 242, 242);
+  width: 100vw;
+  padding: 0.5rem;
+  font-size: 17px;
+}
+
+.navElement {
+  font-weight: 400;
+  font-size: 18pt;
+  margin-left: 1rem;
+}
+
+</style>
